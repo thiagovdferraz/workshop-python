2abf3cca51734b0a3f3d305d540700c30666683d
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-18 23:08:01 -0300

    alterar aula01

--- aula01-inicio/aula01.py
+++ aula01-inicio/aula01.py
@@ -1,1 +1,12 @@
-print ("aula 01")
+print("Algum texto aqui...")
+
+# mostrar para o usuário o texto que queremos
+
+# concatenar string
+
+print("Algum texto aqui..." + " ou aqui...")
+
+
+# comando de input
+user_input = input("digitre seu nome: ")
+print("Olá " + user_input)
