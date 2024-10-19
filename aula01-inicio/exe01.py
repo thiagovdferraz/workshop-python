2f56d44849132badb1a797df6dd86f5ab5c41609
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-18 22:48:40 -0300

    conhecer variaveis e tipos dos dados

---
+++ aula01-inicio/exe01.py
@@ -0,0 +1,4 @@
+# o usuuário digita seu nome e retorna o número de caracteres
+
+user_name = input("Digite seu nome: ")
+print("Seu nome tem " + str(len(user_name)) + " catacteres.")
---
+++ aula01-inicio/exe02.py
@@ -0,0 +1,15 @@
+# soma de dois valores
+
+num1 = int(input("digite o número 1: "))
+num2 = int(input("digite o número 2: "))
+
+soma = num1 + num2
+
+print(f"A soma dos números é: {soma}")
+print(f"Tipo da variável: {type(soma)}")
+# variáveis tipagem
+
+a = 3 # int
+b = 4.78 # float
+c = "Thiago" # string
+d = True # booleano
