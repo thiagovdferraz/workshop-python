9f684fc383778f1783b78770707b836d944e9ccc
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-18 23:14:20 -0300

    adicionar explicações git

--- aula01-inicio/aula01.py
+++ aula01-inicio/aula01.py
@@ -1,12 +1,37 @@
-print("Algum texto aqui...")
+print('Olá!')
 
-# mostrar para o usuário o texto que queremos
+print("segunda alteração...")
 
-# concatenar string
+# git config --global user.name "Seu Nome"
+# git config --global user.email "seu.email@exemplo.com"
+# git config --global --list
 
-print("Algum texto aqui..." + " ou aqui...")
+# git init
+# git status
+# git add file
+# git commit -m "mensagem"
+# git log
+# git restore file - volta para o último commit feito
 
+print("terceira alteração...")
+print("quarta modificação...")
+print("quinta modificação...")
 
-# comando de input
-user_input = input("digitre seu nome: ")
-print("Olá " + user_input)
+#git commit --amend: reescreve a ultima mensagem do ultimo commit
+
+# git checkout hash_num: volta paraq o estado daquele momento
+
+# o que é branch
+# Branches são como linhas do tempo paralelas no seu repositório.
+# Elas permitem que você trabalhe em diferentes funcionalidades ou correções de bugs sem afetar a main.
+
+# git merge
+# apos realizar alterações em outro branch - task1, como podemos jogar em produção?
+# usamos o git merge ticket-1
+
+# passos
+# git checkout main
+# git merge task-1
+
+# deletar ticket-1, pois nao precisamos mais dele
+# git branch -d ticket-1
--- aula01-inicio/aula02.py
+++
@@ -1,1 +0,0 @@
-# meu segundo arquivo
