205c30e37fcddc7d2fed067e738af200a78f03bc
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-19 21:31:13 -0300

    adicionar exercicio 04: divisao por inteiro

---
+++ aula02-python/exercicios.py
@@ -0,0 +1,25 @@
+nome = "thiago"
+print(type(nome))
+
+# upper()
+# lower()
+# strip(): remover espaços em branco
+# split(sep): divide string numa lista
+# +
+
+string = ' thiago@123.com'
+print(string.upper())
+print(string.lower())
+print(string.strip())
+print(string.split("@"))
+
+# Exercícios
+# exe04
+
+print("exercicio 04")
+numero1 = int(input("inserir um numero inteiro: "))
+numero2 = int(input("inserir um numero inteiro: "))
+
+resultado = numero1 // numero2
+
+print(resultado)
