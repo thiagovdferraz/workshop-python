08a115c08e1d9a95c82c183162d7187a189fb00e
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-19 21:45:02 -0300

    adicionar impressao data dia-mes-ano

--- aula02-python/exercicios.py
+++ aula02-python/exercicios.py
@@ -7,31 +7,41 @@ print(type(nome))
 # split(sep): divide string numa lista
 # +
 
-string = ' thiago@123.com'
-print(string.upper())
-print(string.lower())
-print(string.strip())
-print(string.split("@"))
+# string = ' thiago@123.com'
+# print(string.upper())
+# print(string.lower())
+# print(string.strip())
+# print(string.split("@"))
 
-# Exercícios
-# exe04
+# # Exercícios
+# # exe04
 
-print("exercicio 04")
-numero1 = int(input("inserir um numero inteiro: "))
-numero2 = int(input("inserir um numero inteiro: "))
+# print("exercicio 04")
+# numero1 = int(input("inserir um numero inteiro: "))
+# numero2 = int(input("inserir um numero inteiro: "))
 
-resultado = numero1 // numero2
+# resultado = numero1 // numero2
 
-print(resultado)
+# print(resultado)
 
 # Exercícios
 # exe10
 
-print("exercicio 10? area do circulo")
-raio_circulo = float(input("inserir o raio: "))
+# print("exercicio 10? area do circulo")
+# raio_circulo = float(input("inserir o raio: "))
 
-import math
-area = math.pi * raio_circulo ** 2
+# import math
+# area = math.pi * raio_circulo ** 2
 
-print("area do circulo e: ")
-print(area)
+# print("area do circulo e: ")
+# print(f"{area:.2f}")
+
+# exe 15
+
+# data = input("insira uma data no formato dd/mm/aaaa: ")
+data = "10/02/1990"
+lista_dia_mes_ano = data.split("/")
+print(lista_dia_mes_ano)
+print(f"o elemento 1 e o: {lista_dia_mes_ano[0]}")
+print(f"o elemento 2 e o: {lista_dia_mes_ano[1]}")
+print(f"o elemento 3 e o: {lista_dia_mes_ano[2]}")
