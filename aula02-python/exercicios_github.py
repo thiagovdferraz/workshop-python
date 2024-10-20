b46e5f43f0306a14dd0f85477512901aab2b546c
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-19 22:39:38 -0300

    fazer exercicios 1-5

---
+++ aula02-python/exercicios_github.py
@@ -0,0 +1,28 @@
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+    # number_1 = int(input("entre com um numero: "))
+    # number_2 = int(input("entre com um numero: "))
+
+    # add = number_1 + number_2
+    # print(f"a soma e: {add}")
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+    # number_1 = int(input("entre com um numero: "))
+    # resto = number_1 % 5
+    # print(f"o resto da divisao e: {resto}")
+
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+    # number_1 = float(input("entre com um numero: "))
+    # number_2 = float(input("entre com um numero: "))
+    # prod = number_1 * number_2
+    # print(f"o produto e: {prod}")
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+    # number_1 = int(input("entre com um numero inteiro: "))
+    # number_2 = int(input("entre com um numero inteiro: "))
+    # div = number_1 // number_2
+    # print(f"a divisao inteira e: {div}")
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+number_1 = int(input("entre com um numero inteiro: "))
+quadrado = number_1**2
+print(f"o quadrado e: {quadrado}")
