4f84e0091095cc0102da4048a7ce17932bc93370
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-19 22:59:04 -0300

    fazer exercicios strings do 11 ao 15

---
+++ aula02-python/exercicios_11a15.py
@@ -0,0 +1,21 @@
+# Strings (str)
+
+# Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# texto = input("entre com um texto: ")
+# print(f"{texto.upper()}")
+
+# Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# print(f"{texto.lower()}")
+
+# Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# print(f"{texto.strip()}")
+# Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# texto = input("entre com uma data no formato dd/mm/aaaa: ")
+# print(f"o dia e: {texto.split("/")[0]}")
+# print(f"o mes e: {texto.split("/")[1]}")
+# print(f"o ano e: {texto.split("/")[2]}")
+# Escreva um programa que concatene duas strings fornecidas pelo usuário.
+texto1 = input("entre com primeiro nome: ")
+texto2 = input("entre com ultimo nome: ")
+texto_completo = texto1 + " " + texto2
+print(texto_completo)
