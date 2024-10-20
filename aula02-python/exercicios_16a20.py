ca7427ef71596de09d1b95a5438353fe905651f2
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-19 23:20:24 -0300

    incluir exercicios sobre booleanos: 16 a 20

---
+++ aula02-python/exercicios_16a20.py
@@ -0,0 +1,45 @@
+# Booleanos (bool)
+
+# Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# texto1 = input("entre com uma letra ou palavra: ")
+# texto2 = input("entre com outra letra ou palavra: ")
+
+# if texto1 == texto2:
+#     print("palavras iguais")
+# else:
+#     print("palavras diferentes")
+
+# Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# texto1 = bool(input("entre com True or False: "))
+# texto2 = bool(input("entre com True or False: "))
+
+# teste = texto1 or texto2
+# print(teste)
+
+# Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+# texto1 = bool(input("entre com True or False: "))
+
+# teste = not texto1
+# print(teste)
+    # if texto1 == True:
+    #     print(False)
+    # elif texto1 == False:
+    #     print(True)
+
+# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# num_01 = int(input("entre com um numero inteiro: "))
+# num_02 = int(input("entre com um numero inteiro: "))
+
+# if num_01 == num_02:
+#     print("numeros iguais")
+# else: print("numeros diferentes")
+# Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+num_01 = int(input("entre com um numero inteiro: "))
+num_02 = int(input("entre com um numero inteiro: "))
+
+if num_01 != num_02:
+    print("numeros diferentes")
+else: print("numeros numeros iguais")
